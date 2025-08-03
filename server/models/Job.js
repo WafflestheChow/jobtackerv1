8955734aa035a497d54b39afa5be858d710ee07c
Nick <nick.tsang5@gmail.com>
2025-08-03 17:19:03 -0400

    Create Job model with basic schema

---
+++ server/models/Job.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    position: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'interview', 'declined', 'accepted'],
+        default: 'pending'
+    },
+    appliedDate: {
+        type: Date,
+        default: Date.now,
+    },
+    lastUpdated: {
+        type: Date,
+        default: null,
+    },
+    comments: {
+        type: String,
+        trim: true,
+        default: '',
+    }
+
+});
